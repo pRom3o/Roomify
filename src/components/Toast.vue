0a746e83b802821ff22c95a83a7b1fbdefd6254f
pRom3o <alameleromeo1@gmail.com>
2025-11-05 10:41:23 +0100

    amend T&C and toast

--- src/components/Toast.vue
+++ src/components/Toast.vue
@@ -8,7 +8,7 @@ import { toast_message, toast_type, toast_visible } from '../services/toastServi
         ><div
             v-if="toast_visible"
             :class="[
-                'fixed top-5 left-36 z-50 md:left-[46%] px-4 py-2 rounded-2xl  transition-all',
+                'fixed top-5 left-1/2 z-50 -translate-x-1/2 px-4 py-2 rounded-2xl  transition-all shadow-xl',
                 toast_type === 'success'
                     ? 'bg-green-200 border border-green-400'
                     : 'bg-red-200 border border-red-400',
@@ -24,7 +24,7 @@ import { toast_message, toast_type, toast_visible } from '../services/toastServi
 .toast-leave-to {
     opacity: 0;
     transform: scale(0.2);
-    transform: translateY(-30px);
+    transform: translate(-50%, -20px);
 }
 
 .toast-enter-active,
