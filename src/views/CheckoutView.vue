86b6d8036ee08831c4bf4e6bad6f4482bf7b813f
pRom3o <alameleromeo1@gmail.com>
2025-11-11 13:49:26 +0100

    amend

--- src/views/CheckoutView.vue
+++ src/views/CheckoutView.vue
@@ -59,7 +59,7 @@ const handleCheckout = async () => {
                     callback_url: 'https://celines-treats.vercel.app/payment-success',
                 }),
             });
-            console.log('res', response);
+            console.log('res', await response.json());
 
             const paystackResponse = await response.json();
             console.log('response', paystackResponse.data);
