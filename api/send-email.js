234c7c1f6b067c9a4272ff84d12daddd470a5128
pRom3o <alameleromeo1@gmail.com>
2025-11-12 08:12:50 +0100

    amend

--- api/send-email.js
+++ api/send-email.js
@@ -32,7 +32,7 @@ export default async function handler(req, res) {
 
         res.status(200).json({ success: true });
     } catch (error) {
-        console.error(error);
+        console.error('error', error);
         res.status(500).json({ error: 'Email not sent' });
     }
 }
